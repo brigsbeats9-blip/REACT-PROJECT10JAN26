5a2a63e318a3824caa170fed60f7b53be41ce5fb
brigsbeats9-blip <brigsbeats9@gmail.com>
2026-01-14 14:35:10 -0700

    Update React routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import "./App.css";
+import Home from "./Pages/Home";
+import "./index.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Books from "./Pages/Books";
 
+//import { library } from "@fortawesome/fontawesome-svg-core";
+//import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
+//library.add(faBars);
 
 //import Footer from "./components/Footer/Footer";
 //import { BrowerRouter as Router, Route, Routes } from "react-router-dom";
@@ -9,23 +20,22 @@ import Nav from "./components/Nav";
 //import { books } from "./data/books";
 //import BookInfo from "./pages/BookInfo/BookInfo";
 
-/*function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
-}
-
-export default App;*/
-
 function App() {
   return (
-    <div style={{ background: 'red', color: 'white', padding: '40px' }}>
-      APP IS RENDERING
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+
+        <Routes>
+          
+          <Route path="/" exact component={Home} />
+          <Route path="/books" component={Books} />
+          
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,14 +8,59 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faBars);
 
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(*/
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {faBars, faBolt, faBookOpen, faShoppingCart, faStar, faTags, faTimes, faStarHalfAlt} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+Old React (≤17) had a toy called render()
+
+New React (18+) threw that toy away
+
+React 18 only plays with createRoot()
+
+So when you say:
+
+“Hey React, use render()”
+
+React 18 says:
+
+“I don’t have that anymore 😕”
+
+Hence the error.
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
